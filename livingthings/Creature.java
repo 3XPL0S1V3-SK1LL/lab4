76a7e178b861610c956d76754dec7d9b8d69c13b
3XPL0S1V3-SK1LL <153466684+3XPL0S1V3-SK1LL@users.noreply.github.com>
2024-01-19 16:04:34 +0300

    Add files via upload

---
+++ livingthings/Creature.java
@@ -0,0 +1,25 @@
+package livingthings;
+import livingthings.abilities.*;
+import java.util.ArrayList;
+
+public abstract class Creature {
+    public ArrayList<String> favoriteFood = new ArrayList<String>();
+    public ArrayList<String> FoodItHas = new ArrayList<String>();
+    public int waterLvl;
+    public enum Sex{
+        MALE("мальчик"),
+        FEMALE("девочка");
+        private final String value;
+        private Sex(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+    public abstract void eat(String food);
+    public abstract void drinkWater();
+
+
+}
