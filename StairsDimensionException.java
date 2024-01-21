66a9ca8bcc74fe4024d2dec4f0cba0dd0c80ac93
3XPL0S1V3-SK1LL <153466684+3XPL0S1V3-SK1LL@users.noreply.github.com>
2024-01-21 09:05:57 +0300

    Delete StairsDimensionException.java

--- StairsDimensionException.java
+++
@@ -1,7 +0,0 @@
-package builtinexception;
-
-public class StairsDimensionException extends RuntimeException {
-    public StairsDimensionException(String str){
-        super("Лестница слишком" + str);
-    }
-}
