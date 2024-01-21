8a9ea9164832d4d199370eb72290a1981388b593
3XPL0S1V3-SK1LL <153466684+3XPL0S1V3-SK1LL@users.noreply.github.com>
2024-01-21 09:06:05 +0300

    Delete StairsDirectionException.java

--- StairsDirectionException.java
+++
@@ -1,7 +0,0 @@
-package builtinexception;
-
-public class StairsDirectionException extends RuntimeException {
-    public StairsDirectionException(String str){
-        super("Лестаница ведет в одно и то же место в - " + str);
-    }
-}
