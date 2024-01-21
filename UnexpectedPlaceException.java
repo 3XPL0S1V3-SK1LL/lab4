d4e4c82577da81a8c403abf35566b752ff210778
3XPL0S1V3-SK1LL <153466684+3XPL0S1V3-SK1LL@users.noreply.github.com>
2024-01-21 09:06:11 +0300

    Delete UnexpectedPlaceException.java

--- UnexpectedPlaceException.java
+++
@@ -1,7 +0,0 @@
-package builtinexception;
-
-public class UnexpectedPlaceException extends RuntimeException{
-    public UnexpectedPlaceException(String expectingPlace, String currentPlace) {
-        super("Место положение должно быть " + expectingPlace + ", а сейчас " + currentPlace);
-    }
-}
