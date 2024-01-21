ebf6c2f15d6ec3e8bee357af446678c095c8017b
3XPL0S1V3-SK1LL <153466684+3XPL0S1V3-SK1LL@users.noreply.github.com>
2024-01-21 09:05:50 +0300

    Delete InvalidDestinationException.java

--- InvalidDestinationException.java
+++
@@ -1,7 +0,0 @@
-package builtinexception;
-
-public class InvalidDestinationException extends RuntimeException {
-    public InvalidDestinationException(String str) {
-        super("Вы и так находитесь в этом месте! (" + str + " )" );
-    }
-}
